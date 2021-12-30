e290d77cd4e62cf2119fe0dce9aadf040ea9d592
mujeeb <muzeeb100@yahoo.co.in>
2021-12-30 23:40:13 +0530

    first commit

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,32 @@
+import "./topbar.scss";
+import { Person, Mail } from "@material-ui/icons";
+
+export default function Topbar({menuOpen, setMenuOpen}) {
+  return (
+    <div className={"topbar "+ (menuOpen && 'active')}>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            {" "}
+            genuis.
+          </a>
+          <div className="itemContainer">
+            <Person className="icon" />
+            <span>8097749932</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>cloudage.software.solutions@gmail.com</span>
+          </div>
+        </div>
+        <div className="right">
+        <div className="hamburger" onClick={() =>setMenuOpen(!menuOpen)}>
+          <span className="line1"></span>
+          <span className="line2"></span>
+          <span className="line3"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
